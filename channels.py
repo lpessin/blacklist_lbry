72d2765edbb4fcd203229812e759589ec6aedf6d
Luiz <34900176+lpessin@users.noreply.github.com>
2020-05-12 00:09:29 -0300

    Create channels.py

---
+++ channels.py
@@ -0,0 +1,24 @@
+import requests
+import csv
+
+names = []
+with open('channels', 'r') as r:
+    reader = csv.reader(r)
+    for row in reader:
+        names.append(row[0])
+
+claim_ids = []
+for name in names:
+    url = f'lbry://{name}'
+    call = requests.post("http://localhost:5279", json={"method": "resolve",
+                                                        "params": {"urls": [url]}}).json().get('result').get(url)
+    id = call.get('claim_id')
+    claim_ids.append(id)
+
+print(claim_ids)
+print(len(claim_ids))
+
+with open('ids', 'w', newline='') as ids:
+    write = csv.writer(ids)
+    for i in claim_ids:
+        write.writerow([i])
